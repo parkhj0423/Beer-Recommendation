ec7d3bcadb508ef2e5d2f07b5e9db4cceb691931
Harry <harry@catius.io>
2022-07-27 11:47:24 +0900

    Initial Commit

---
+++ Beer-Recommendation/Beer_RecommendationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beer_RecommendationApp.swift
+//  Beer-Recommendation
+//
+//  Created by Catius_Macbook_Air on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct Beer_RecommendationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
