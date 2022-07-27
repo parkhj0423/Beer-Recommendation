a9c565919de3d899eca1764374acfef214ac7dc6
parkhj0423 <parkhj0423@naver.com>
2022-07-27 14:06:57 +0900

    [parkhj0423] 프로젝트 생성 및 sourcetree와 연동 완료

--- Beer-Recommendation/ContentView.swift
+++ Beer-Recommendation/ContentView.swift
@@ -9,9 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        // MVVM, SwiftUI, Combine, Clean Architecture를 사용하여 아키텍쳐에 대해 파볼 예정
         Text("Beer!")
             .padding()
-        
     }
 }
 
